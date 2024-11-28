0c966b68365892e4305841dc0a00c43dc59cfa7c
BohdanMoldavets <143205638+BohdanMoldavets@users.noreply.github.com>
2024-11-28 18:41:57 +0100

    Add java config bean

---
+++ 02-spring-boot-spring-core/09-java-config-bean/src/main/java/com/moldavets/springcoredemo/common/BaseballCoach.java
@@ -0,0 +1,16 @@
+package com.moldavets.springcoredemo.common;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class BaseballCoach implements Coach {
+
+    public BaseballCoach() {
+        System.out.println("In constructor: " + getClass().getSimpleName());
+    }
+
+    @Override
+    public String getDailyWorkout() {
+        return "Practice baseball for 30 minutes";
+    }
+}
