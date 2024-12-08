267b918d5e188362bd92e3ea1cc7aa8b6543c5c2
BohdanMoldavets <143205638+BohdanMoldavets@users.noreply.github.com>
2024-12-08 19:54:08 +0100

    Add primitive spring mvc project

---
+++ 06-spring-boot-spring-mvc/01-thymeleafdemo-helloworld/src/main/java/com/moldavets/springboot/thymeleafdemo/controller/DemoController.java
@@ -0,0 +1,17 @@
+package com.moldavets.springboot.thymeleafdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class DemoController {
+
+    @GetMapping("/hello")
+    public String helloWorld(Model model) {
+        model.addAttribute("theDate", java.time.LocalDate.now());
+
+        return "helloWorld";
+    }
+
+}
