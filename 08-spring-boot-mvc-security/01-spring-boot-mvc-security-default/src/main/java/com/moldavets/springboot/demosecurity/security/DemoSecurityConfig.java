7b6b266bacf077bb94bd5d0b03ce8b3e174f1e8c
BohdanMoldavets <143205638+BohdanMoldavets@users.noreply.github.com>
2024-12-12 22:22:36 +0100

    Add custom login page

--- 08-spring-boot-mvc-security/01-spring-boot-mvc-security-default/src/main/java/com/moldavets/springboot/demosecurity/security/DemoSecurityConfig.java
+++ 08-spring-boot-mvc-security/01-spring-boot-mvc-security-default/src/main/java/com/moldavets/springboot/demosecurity/security/DemoSecurityConfig.java
@@ -2,9 +2,12 @@ package com.moldavets.springboot.demosecurity.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class DemoSecurityConfig {
@@ -31,4 +34,19 @@ public class DemoSecurityConfig {
         return new InMemoryUserDetailsManager(john, susan, katy);
     }
 
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(cofigurer ->
+                cofigurer
+                        .anyRequest()
+                        .authenticated()
+        ).formLogin(form ->
+                form
+                    .loginPage("/showLoginPage")
+                    .loginProcessingUrl("/authenticateTheUser").permitAll()
+        )
+                .logout(LogoutConfigurer::permitAll
+        );
+        return http.build();
+    }
 }
