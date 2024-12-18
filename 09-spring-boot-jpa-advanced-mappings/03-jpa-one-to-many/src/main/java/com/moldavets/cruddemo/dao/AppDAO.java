7f57dbdd7d6d49ee6e3261d35773f76120f48b7f
BohdanMoldavets <143205638+BohdanMoldavets@users.noreply.github.com>
2024-12-18 21:25:24 +0100

    Implemet all methods for CRUD operations for 01-jpa-one-to-one-uni

--- 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/moldavets/cruddemo/dao/AppDAO.java
+++ 09-spring-boot-jpa-advanced-mappings/03-jpa-one-to-many/src/main/java/com/moldavets/cruddemo/dao/AppDAO.java
@@ -1,10 +1,14 @@
 package com.moldavets.cruddemo.dao;
 
 import com.moldavets.cruddemo.entity.Instructor;
+import com.moldavets.cruddemo.entity.InstructorDetail;
 
 public interface AppDAO {
 
     void save(Instructor instructor);
     Instructor findInstructorById(int id);
     void deleteInstructorById(int id);
+
+    InstructorDetail findInstructorDetailById(int id);
+    void deleteInstructorDetailById(int id);
 }
